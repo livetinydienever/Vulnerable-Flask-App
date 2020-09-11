59bd6d794c27878e26c8919cfcb22713c5a1762c
livetinydienever <sarah.perkins@whitehatsec.com>
2020-09-10 21:40:19 -0500

    to make pylinter happier

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import session, Flask, jsonify, request, Response, render_template, render_template_string, url_for
+from flask import Flask, jsonify, request, Response, render_template, render_template_string, url_for
 from flask_sqlalchemy import SQLAlchemy
 import jwt
 from jwt.exceptions import DecodeError, MissingRequiredClaimError, InvalidKeyError
@@ -15,7 +15,6 @@ import yaml
 from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
-import os
 import base64
 
 app_port = os.environ.get('APP_PORT', 5050)
@@ -30,27 +29,30 @@ app.config['STATIC_FOLDER'] = None
 
 db = SQLAlchemy(app)
 
+
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.String(80), unique = True)
-    password = db.Column(db.String(80), unique = True)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(80), unique=True)
 
 
-    def __repr__(self):
-        return "<User {0}>".format(self.username)
+def __repr__(self):
+    return "<User {0}>".format(self.username)
+
 
 class Customer(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(80))
     last_name = db.Column(db.String(80))
     email = db.Column(db.String(80))
-    ccn = db.Column(db.String(80), nullable = True)
+    ccn = db.Column(db.String(80), nullable=True)
     username = db.Column(db.String(80))
     password = db.Column(db.String(150))
 
 
-    def __repr__(self):
-        return "<User {0} {1}>".format(self.first_name, self.last_name)
+def __repr__(self):
+    return "<User {0} {1}>".format(self.first_name, self.last_name)
+
 
 @app.before_first_request
 def setup_users():
@@ -64,13 +66,13 @@ def setup_users():
         db.session.add(user)
         db.session.commit()
     if not Customer.query.first():
-        for i in range(0,5):
+        for i in range(0, 5):
             fake = Faker()
             cust = Customer()
             cust.first_name = fake.first_name()
             cust.last_name = fake.last_name()
-            cust.email = fake.simple_profile(sex = None)['mail']
-            cust.username = fake.simple_profile(sex = None)['username']
+            cust.email = fake.simple_profile(sex=None)['mail']
+            cust.username = fake.simple_profile(sex=None)['username']
             cust.password = str(base64.b64encode(os.urandom(16)))
             cust.ccn = fake.credit_card_number(card_type=None)
             db.session.add(cust)
@@ -78,12 +80,14 @@ def setup_users():
 
 
 def get_exp_date():
-    exp_date = datetime.datetime.utcnow() + datetime.timedelta(minutes = 240)
+    exp_date = datetime.datetime.utcnow() + datetime.timedelta(minutes=240)
     return exp_date
+
 
 def verify_jwt(token):
     try:
-        decoded = jwt.decode(token, app.config['SECRET_KEY_HMAC'], verify=True, issuer = 'we45', leeway=10, algorithms=['HS256'])
+        decoded = jwt.decode(token, app.config['SECRET_KEY_HMAC'],
+                             verify=True, issuer='we45', leeway=10, algorithms=['HS256'])
         print("JWT Token from API: {0}".format(decoded))
         return True
     except DecodeError:
@@ -93,10 +97,12 @@ def verify_jwt(token):
         print('Claim required is missing: {0}'.format(e))
         return False
 
+
 def insecure_verify(token):
-    decoded = jwt.decode(token, verify = False)
+    decoded = jwt.decode(token, verify=False)
     print(decoded)
     return True
+
 
 @app.errorhandler(404)
 def pnf(e):
@@ -111,14 +117,16 @@ def pnf(e):
     </html>
     ''' % request.url
 
-    return render_template_string(template, dir = dir, help = help, locals = locals),404
+    return render_template_string(template, dir=dir, help=help, locals=locals), 404
+
 
 def has_no_empty_params(rule):
     default = rule.defaults if rule.defaults is not None else ()
     arguments = rule.arguments if rule.arguments is not None else ()
     return len(default) >= len(arguments)
 
-@app.route('/', methods = ['GET'])
+
+@app.route('/', methods=['GET'])
 def sitemap():
     links = []
     for rule in app.url_map.iter_rules():
@@ -128,10 +136,10 @@ def sitemap():
                 url = url_for(rule.endpoint, **(rule.defaults or {}))
                 links.append((url, rule.endpoint, ','.join(rule.methods)))
 
-    return render_template('index.html', urls = links)
+    return render_template('index.html', urls=links)
 
 
-@app.route('/register/user', methods = ['POST'])
+@app.route('/register/user', methods=['POST'])
 def reg_customer():
     try:
         content = request.json
@@ -143,11 +151,12 @@ def reg_customer():
             db.session.add(new_user)
             db.session.commit()
             user_created = 'User: {0} has been created'.format(username)
-            return jsonify({'Created': user_created}),200
+            return jsonify({'Created': user_created}), 200
     except Exception as e:
-        return jsonify({'Error': str(e.message)}),404
+        return jsonify({'Error': str(e.message)}), 404
 
-@app.route('/register/customer', methods = ['POST'])
+
+@app.route('/register/customer', methods=['POST'])
 def reg_user():
     try:
         content = request.json
@@ -162,12 +171,12 @@ def reg_user():
             db.session.add(cust)
             db.session.commit()
             user_created = 'Customer: {0} has been created'.format(username)
-            return jsonify({'Created': user_created}),200
+            return jsonify({'Created': user_created}), 200
     except Exception as e:
-        return jsonify({'Error': str(e.message)}),404
+        return jsonify({'Error': str(e.message)}), 404
 
 
-@app.route('/login', methods = ['POST'])
+@app.route('/login', methods=['POST'])
 def login():
     '''
     You will need to authenticate to this URI first. You will need to pass a JSON body with a username and password key.
@@ -179,9 +188,10 @@ def login():
         print(content)
         username = content['username']
         password = content['password']
-        auth_user = User.query.filter_by(username = username, password = password).first()
+        auth_user = User.query.filter_by(username=username, password=password).first()
         if auth_user:
-            auth_token = jwt.encode({'user': username, 'exp': get_exp_date(), 'nbf': datetime.datetime.utcnow(), 'iss': 'we45', 'iat': datetime.datetime.utcnow()}, app.config['SECRET_KEY_HMAC'], algorithm='HS256')
+            auth_token = jwt.encode({'user': username, 'exp': get_exp_date(), 'nbf': datetime.datetime.utcnow(
+            ), 'iss': 'we45', 'iat': datetime.datetime.utcnow()}, app.config['SECRET_KEY_HMAC'], algorithm='HS256')
             resp = Response(json.dumps({'Authenticated': True, "User": username}))
             #resp.set_cookie('SESSIONID', auth_token)
             resp.headers['Authorization'] = "{0}".format(auth_token)
@@ -189,18 +199,19 @@ def login():
             resp.mimetype = 'application/json'
             return resp
         else:
-            return jsonify({'Error': 'No User here...'}),404
+            return jsonify({'Error': 'No User here...'}), 404
     except:
-        return jsonify({'Error': 'Unable to recognize Input'}),404
+        return jsonify({'Error': 'Unable to recognize Input'}), 404
 
-@app.route('/fetch/customer', methods = ['POST'])
+
+@app.route('/fetch/customer', methods=['POST'])
 def fetch_customer():
     token = request.headers.get('Authorization')
     if not token:
-        return jsonify({'Error': 'Not Authenticated!'}),403
+        return jsonify({'Error': 'Not Authenticated!'}), 403
     else:
         if not verify_jwt(token):
-            return jsonify({'Error': 'Invalid Token'}),403
+            return jsonify({'Error': 'Invalid Token'}), 403
         else:
             content = request.json
             if content:
@@ -209,16 +220,16 @@ def fetch_customer():
                 customer_dict = {'id': customer_record.id, 'firstname': customer_record.first_name,
                                  'lastname': customer_record.last_name, 'email': customer_record.email,
                                  'cc_num': customer_record.ccn, 'username': customer_record.username
-                                }
+                                 }
                 if customer_record:
-                    return jsonify(customer_dict),200
+                    return jsonify(customer_dict), 200
                 else:
-                    return jsonify({'Error': 'No Customer Found'}),404
+                    return jsonify({'Error': 'No Customer Found'}), 404
             else:
-                return jsonify({'Error': 'Invalid Request'}),400
+                return jsonify({'Error': 'Invalid Request'}), 400
 
 
-@app.route('/get/<cust_id>', methods = ['GET'])
+@app.route('/get/<cust_id>', methods=['GET'])
 def get_customer(cust_id):
     token = request.headers.get('Authorization')
     if not token:
@@ -231,26 +242,24 @@ def get_customer(cust_id):
                 customer_record = Customer.query.get(cust_id)
                 if customer_record:
                     customer_dict = {'id': customer_record.id, 'firstname': customer_record.first_name,
-                                 'lastname': customer_record.last_name, 'email': customer_record.email,
-                                 'cc_num': customer_record.ccn, 'username': customer_record.username
-                                }
-                    return jsonify(customer_dict),200
+                                     'lastname': customer_record.last_name, 'email': customer_record.email,
+                                     'cc_num': customer_record.ccn, 'username': customer_record.username
+                                     }
+                    return jsonify(customer_dict), 200
                 else:
-                    return jsonify({'Error': 'No Customer Found'}),404
+                    return jsonify({'Error': 'No Customer Found'}), 404
             else:
-                return jsonify({'Error': 'Invalid Request'}),400
+                return jsonify({'Error': 'Invalid Request'}), 400
 
 
-
-
-@app.route('/search', methods = ['POST'])
+@app.route('/search', methods=['POST'])
 def search_customer():
     token = request.headers.get('Authorization')
     if not token:
-        return jsonify({'Error': 'Not Authenticated!'}),403
+        return jsonify({'Error': 'Not Authenticated!'}), 403
     else:
         if not verify_jwt(token):
-            return jsonify({'Error': 'Invalid Token'}),403
+            return jsonify({'Error': 'Invalid Token'}), 403
         else:
             content = request.json
             results = []
@@ -266,7 +275,7 @@ def search_customer():
                     for result in search_query:
                         results.append(list(result))
                     print(results)
-                    return jsonify(results),200
+                    return jsonify(results), 200
                 except Exception as e:
                     template = '''<html>
                         <head>
@@ -302,17 +311,19 @@ def hello():
         # Access saved file
         document = Document(file_path)
         for para in document.paragraphs:
-            print (para.text)  # '\n\n'.join([para.text for paragraph in document.paragraphs])
+            print(para.text)  # '\n\n'.join([para.text for paragraph in document.paragraphs])
 
     # return "file uploaded successfully"
     return render_template('view.html', name=para.text)
+
 
 @app.route("/yaml")
 def yaml_upload():
     return render_template(
         'yaml_test.html')
 
-@app.route("/yaml_hammer", methods = ['POST'])
+
+@app.route("/yaml_hammer", methods=['POST'])
 def yaml_hammer():
     if request.method == "POST":
         f = request.files['file']
@@ -328,8 +339,7 @@ def yaml_hammer():
 
         ydata = yaml.load(y)
 
-    return render_template('view.html', name = json.dumps(ydata))
-
+    return render_template('view.html', name=json.dumps(ydata))
 
 
 if __name__ == "__main__":
